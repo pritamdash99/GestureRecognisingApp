d914effc4fc40fe945a365ad3cd514918ec6127b
Pritam Dash <pritamdash2015@outlook.com>
2022-06-15 22:45:09 +0530

    Initial Commit

---
+++ GestureRecognisingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognisingApp
+//
+//  Created by Pritam Dash on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
